f0501d55d09910ad55451f8f8bf927ae66b85a69
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-25 22:38:15 +0200

    [imgui.hpp] Update

--- include/cmd/imgui.hpp
+++ include/cmd/imgui.hpp
@@ -5,19 +5,23 @@
 namespace cmd {
 
 template<typename CommandToString, Command CommandT>
-void imgui_show_history(History<CommandT> history, const CommandToString& command_to_string)
+void imgui_show_history(const History<CommandT>& history, const CommandToString& command_to_string)
 {
     const auto& commands = history.underlying_container();
-    size_t      i        = 0u;
-    for (const auto& command : commands) {
-        if (i == history.current_command_index()) {
+    bool        drawn    = false;
+    for (auto it = commands.cbegin(); it != commands.cend(); ++it) {
+        if (it == history.current_command_iterator()) {
+            drawn = true;
             ImGui::Separator();
         }
-        ImGui::Text("%s", command_to_string(command).c_str());
-        i++;
+        ImGui::Text("%s", command_to_string(*it).c_str());
     }
-    if (i == history.current_command_index()) {
+    if (!drawn) {
         ImGui::Separator();
+    }
+    using namespace std::chrono_literals;
+    if (history.time_since_last_push() < 100ms) {
+        ImGui::SetScrollHereY(1.0f);
     }
 }
 
