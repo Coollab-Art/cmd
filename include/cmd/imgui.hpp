0e54b6b5fcfb57c59af2185c67f36e85c0bbacf2
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-30 02:54:51 +0200

    🎨 Apply .clang-format

--- include/cmd/imgui.hpp
+++ include/cmd/imgui.hpp
@@ -11,8 +11,7 @@ public:
     explicit HistoryWithUi(size_t max_size)
         : History<CommandT>{max_size}
         , _uncommited_max_size{max_size}
-    {
-    }
+    {}
 
     void push(const CommandT& command) override
     {
@@ -29,8 +28,10 @@ public:
     {
         const auto& commands = History<CommandT>::underlying_container();
         bool        drawn    = false;
-        for (auto it = commands.cbegin(); it != commands.cend(); ++it) {
-            if (it == History<CommandT>::current_command_iterator()) {
+        for (auto it = commands.cbegin(); it != commands.cend(); ++it)
+        {
+            if (it == History<CommandT>::current_command_iterator())
+            {
                 drawn = true;
                 ImGui::Separator();
                 using namespace std::chrono_literals;
@@ -41,7 +42,8 @@ public:
             }
             ImGui::Text("%s", command_to_string(*it).c_str());
         }
-        if (!drawn) {
+        if (!drawn)
+        {
             ImGui::Separator();
         }
     }
@@ -50,7 +52,8 @@ public:
     {
         static_assert(sizeof(_uncommited_max_size) == 8, "The ImGui widget expects a u64 integer");
         ImGui::InputScalar(label, ImGuiDataType_U64, &_uncommited_max_size);
-        if (ImGui::IsItemDeactivatedAfterEdit()) {
+        if (ImGui::IsItemDeactivatedAfterEdit())
+        {
             History<CommandT>::set_max_size(_uncommited_max_size);
             return true;
         }
